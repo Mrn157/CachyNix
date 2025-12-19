b53be4aca6f0604867e71c5d90999abc35bcbafa
Mrn157 <gamerzzz1510@gmail.com>
2025-12-19 17:34:57 +1300

    First commit

---
+++ README.md
@@ -0,0 +1,92 @@
+Testing, will update when I have time
+
+<h1>CachyNix</h1>
+
+<p>We recommend integrating this repo using Flakes:</p>
+
+<pre lang="nix"><code class="language-nix">
+# flake.nix
+{
+  description = "My configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    chaotic.url = "github:Mrn157/CachyNix"; # IMPORTANT
+  };
+
+  outputs = { nixpkgs, chaotic, ... }: {
+    nixosConfigurations = {
+      hostname = nixpkgs.lib.nixosSystem { # Replace "hostname" with your system's hostname
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          chaotic.nixosModules.default # IMPORTANT
+        ];
+      };
+    };
+  };
+}
+</code></pre>
+
+<p>In your <code>configuration.nix</code> enable the kernel and options that you prefer:</p>
+
+<pre lang="nix"><code class="language-nix">
+boot = {
+    kernelPackages = pkgs.linuxPackages_cachyos.cachyOverride { mArch = "GENERIC_V3"; };
+    # ...
+};
+</code></pre>
+
+<h3 id="on-home-manager">On Home-Manager</h3>
+
+<p>This method is for home-manager setups <strong>without NixOS</strong>.</p>
+
+<p>We recommend integrating this repo using Flakes:</p>
+
+<pre lang="nix"><code class="language-nix">
+# flake.nix
+{
+  description = "My configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    chaotic.url = "github:Mrn157/CachyNix"; # IMPORTANT
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, chaotic, ... }: {
+    # ... other outputs
+    homeConfigurations = {
+      # ... other configs
+      configName = home-manager.lib.homeManagerConfiguration { # Replace "configName" with a significant unique name
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        modules = [
+          ./home-manager/default.nix
+          chaotic.homeManagerModules.default # IMPORTANT
+        ];
+      };
+    };
+  };
+}
+</code></pre>
+
+<h3>CachyOS kernels</h3>
+
+
+<p>Expect no cache from all </p>
+
+<p>You may install the CachyOS kernel directly using the default modules and overlays with <code>pkgs.linuxPackages_cachyos</code>. Alternatively, use <code>chaotic.legacyPackages.x86_64-linux.linuxPackages_cachyos</code> if you would like to use the package directly without using modules and overlay</p>
+
+<h3>CachyOS x86-64 microarchitecture optimisations</h3>
+
+<pre lang="nix"><code class="language-nix">
+{ pkgs, ... }:
+{
+  boot.kernelPackages = pkgs.linuxPackages_cachyos.cachyOverride { mArch = "GENERIC_V4"; };
+}
+</code></pre>
+
+<p>Use either <code>GENERIC_V2</code>, <code>GENERIC_V3</code>, <code>GENERIC_V4</code>, or <code>ZEN4</code>. We don't provide cache for these.</p>
---
+++ overlays/default.nix
@@ -0,0 +1,148 @@
+# Conventions:
+# - Sort packages in alphabetic order.
+# - If the recipe uses `override` or `overrideAttrs`, then use callOverride,
+#   otherwise use `final`.
+# - Composed names are separated with minus: `lan-mouse`
+# - Versions/patches are suffixed with an underline: `mesa_git`, `libei_0_5`, `linux_hdr`
+
+# NOTE:
+# - `*_next` packages will be removed once merged into nixpkgs-unstable.
+
+{
+  flakes,
+  nixpkgs ? flakes.nixpkgs,
+  self ? flakes.self,
+  selfOverlay ? self.overlays.default,
+  jovian ? flakes.jovian,
+  rust-overlay ? flakes.rust-overlay,
+  nixpkgsExtraConfig ? { },
+}:
+final: prev:
+
+let
+  # Required to load version files.
+  inherit (final.lib.trivial) importJSON;
+
+  # Our utilities/helpers.
+  nyxUtils = import ../shared/utils.nix {
+    inherit (final) lib;
+    nyxOverlay = selfOverlay;
+  };
+  inherit (nyxUtils) multiOverride overrideDescription drvDropUpdateScript;
+
+  # Helps when calling .nix that will override packages.
+  callOverride =
+    path: attrs:
+    import path (
+      {
+        inherit
+          final
+          flakes
+          nyxUtils
+          prev
+          gitOverride
+          rustPlatform_latest
+          ;
+      }
+      // attrs
+    );
+
+  # Helps when calling .nix that will override i686-packages.
+  callOverride32 =
+    path: attrs:
+    import path (
+      {
+        inherit flakes nyxUtils gitOverride;
+        final = final.pkgsi686Linux;
+        final64 = final;
+        prev = prev.pkgsi686Linux;
+      }
+      // attrs
+    );
+
+  # Magic helper for _git packages.
+  gitOverride = import ../shared/git-override.nix {
+    inherit (final)
+      lib
+      callPackage
+      fetchFromGitHub
+      fetchFromGitLab
+      fetchFromGitea
+      ;
+    inherit (final.rustPlatform) fetchCargoVendor;
+    nyx = self;
+    fetchRevFromGitHub = final.callPackage ../shared/github-rev-fetcher.nix { };
+    fetchRevFromGitLab = final.callPackage ../shared/gitlab-rev-fetcher.nix { };
+    fetchRevFromGitea = final.callPackage ../shared/gitea-rev-fetcher.nix { };
+  };
+
+  rustc_latest = rust-overlay.packages.${final.stdenv.hostPlatform.system}.rust;
+
+  # Latest rust toolchain from Fenix
+  rustPlatform_latest = final.makeRustPlatform {
+    cargo = rustc_latest;
+    rustc = rustc_latest;
+  };
+
+  # Too much variations
+  cachyosPackages = callOverride ../linux-cachyos { };
+
+  # Microarch stuff
+  makeMicroarchPkgs = import ../shared/make-microarch.nix {
+    inherit
+      nixpkgs
+      final
+      selfOverlay
+      nixpkgsExtraConfig
+      ;
+  };
+
+  # Required for 32-bit packages
+  has32 = final.stdenv.hostPlatform.isLinux && final.stdenv.hostPlatform.isx86;
+
+  # Required for kernel packages
+  inherit (final.stdenv) isLinux;
+
+  # Apply Jovian overlay only on x86_64-linux
+  jovian-chaotic =
+    if final.stdenv.hostPlatform.isLinux && final.stdenv.hostPlatform.isx86_64 then
+      let
+        base = nyxUtils.applyOverlay {
+          overlay = jovian.overlays.default;
+          replace = true;
+          pkgs = prev;
+        };
+      in
+      (builtins.removeAttrs base [ "jovian-documentation" ])
+      // {
+        recurseForDerivations = true;
+        linuxPackages_jovian = base.linuxPackages_jovian // {
+          recurseForDerivations = false;
+        };
+      }
+    else
+      { };
+in
+{
+  inherit nyxUtils jovian-chaotic rustc_latest;
+
+  linux_cachyos = drvDropUpdateScript cachyosPackages.cachyos-gcc.kernel;
+  linux_cachyos-lto = drvDropUpdateScript cachyosPackages.cachyos-lto.kernel;
+  linux_cachyos-lto-znver4 = drvDropUpdateScript cachyosPackages.cachyos-lto-znver4.kernel;
+  linux_cachyos-gcc = drvDropUpdateScript cachyosPackages.cachyos-gcc.kernel;
+  linux_cachyos-server = drvDropUpdateScript cachyosPackages.cachyos-server.kernel;
+  linux_cachyos-hardened = drvDropUpdateScript cachyosPackages.cachyos-hardened.kernel;
+  linux_cachyos-rc = cachyosPackages.cachyos-rc.kernel;
+  linux_cachyos-lts = cachyosPackages.cachyos-lts.kernel;
+
+  linuxPackages_cachyos = cachyosPackages.cachyos-gcc;
+  linuxPackages_cachyos-lto = cachyosPackages.cachyos-lto;
+  linuxPackages_cachyos-lto-znver4 = cachyosPackages.cachyos-lto-znver4;
+  linuxPackages_cachyos-gcc = cachyosPackages.cachyos-gcc;
+  linuxPackages_cachyos-server = cachyosPackages.cachyos-server;
+  linuxPackages_cachyos-hardened = cachyosPackages.cachyos-hardened;
+  linuxPackages_cachyos-rc = cachyosPackages.cachyos-rc;
+  linuxPackages_cachyos-lts = cachyosPackages.cachyos-lts;
+
+  zfs_cachyos = cachyosPackages.zfs;
+}
